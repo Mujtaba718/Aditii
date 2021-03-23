8305eb7aa62d0c88f3785f80ad6cf1efbaafffd8
Mujtaba <mujtaba.iq@gmail.com>
2021-03-22 21:58:18 -0400

    Made website more mobile-friendly by using media query along with flexbox and grid.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const menu_btn = document.querySelector(".menu_btn");
+const menu = document.querySelector("nav ul");
 
 function buttonHandler() {
     this.classList.toggle("open");
+    menu.classList.toggle("open_menu");
 }
 
 menu_btn.addEventListener("click", buttonHandler)
